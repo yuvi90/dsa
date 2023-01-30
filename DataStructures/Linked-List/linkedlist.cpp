20b6b98752c6ff54a629ca53a3777c3f2d71e4bb
Yuvraj <ygill63@gmail.com>
2023-01-30 12:07:37 +0530

    linked list insertion practice

--- DataStructures/Linked-List/linkedlist.cpp
+++ DataStructures/Linked-List/linkedlist.cpp
@@ -41,44 +41,85 @@ int searchList(Node *head, int value)
     return 0;
 }
 
-//-->> Insertions (at first, at end, at index)
+//-->> Insertions (at First, at Last, at Index)
+void insertAtFirst(Node *head, int value)
+{
+    Node *newNode = createNode(head->data);
+    newNode->next = head->next;
+    head->data = value;
+    head->next = newNode;
+}
+
+void insertAtLast(Node *head, int value)
+{
+    while (head->next != NULL)
+    {
+        head = head->next;
+    }
+    Node *newNode = createNode(value);
+    head->next = newNode;
+    newNode->next = NULL;
+}
+
+void insertAtIndex(Node *head, int value, int index)
+{
+    int i = 1;
+    while (i < index-1)
+    {
+        i++;
+        head = head->next;
+    }
+    Node *newNode = createNode(value);
+    newNode->next = head->next;
+    head->next = newNode;
+}
+
+//-->> Deletion (at First, at Last, at Index)
 
 int main()
 {
     //-------------------------------------->> Creating and Allocating Memory in Heap
 
     //-->> List1
-    Node *n1 = (Node *)malloc(sizeof(Node));
+    Node *head1 = (Node *)malloc(sizeof(Node));
     Node *n2 = (Node *)malloc(sizeof(Node));
     Node *n3 = (Node *)malloc(sizeof(Node));
     Node *n4 = (Node *)malloc(sizeof(Node));
 
-    n1->data = 2;
+    head1->data = 2;
     n2->data = 4;
     n3->data = 6;
     n4->data = 8;
 
-    n1->next = n2;
+    head1->next = n2;
     n2->next = n3;
     n3->next = n4;
     n4->next = NULL;
 
     //-->> List2
-    Node *m1 = createNode(3);
+    Node *head2 = createNode(3);
     Node *m2 = createNode(6);
     Node *m3 = createNode(9);
     Node *m4 = createNode(12);
 
-    m1->next = m2;
+    head2->next = m2;
     m2->next = m3;
     m3->next = m4;
     m4->next = NULL;
 
-    traverseList(n1);
+    traverseList(head1);
     cout << endl;
-    traverseList(m1);
+    traverseList(head2);
     cout << endl;
-    searchList(m1, 9);
+
+    insertAtFirst(head1, 1);
+    insertAtLast(head1, 10);
+    insertAtIndex(head1, 5, 2);
+    cout << endl;
+    traverseList(head1);
+
+    cout << endl;
+    searchList(head1, 5);
 
     return 0;
 }
