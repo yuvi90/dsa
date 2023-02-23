6a4de07a101a339b7655ef50b5772ff1a83054bb
Yuvraj <itsyuvi@outlook.com>
2023-02-23 12:08:45 +0530

    added Java linked list methods

--- DataStructures/DS_in_Java/src/linearDS/LinkedList.java
+++ DataStructures/DS_in_Java/src/linearDS/LinkedList.java
@@ -1,11 +1,13 @@
 package linearDS;
 
+import java.util.NoSuchElementException;
+
 public class LinkedList {
     class Node {
         private int value;
         private Node next;
 
-        public Node (int data) {
+        public Node(int data) {
             this.value = data;
         }
     }
@@ -15,7 +17,7 @@ public class LinkedList {
     private int size;
 
     // check if list is empty
-    boolean isEmpty () {
+    boolean isEmpty() {
         if (this.first == null) {
             return true;
         }
@@ -53,18 +55,82 @@ public class LinkedList {
         }
     }
 
-    // deleteFirst
+    // deleteFirst --> [ 10 -> 20 -> 30 -> null ]
+    public void deleteFirst() {
+        if (this.isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        if (this.size == 1) {
+            this.first = null;
+            this.last = null;
+            this.size--;
+        } else {
+            this.first = first.next;
+            this.size--;
+        }
+    }
+
     // deleteLast
+    public void deleteLast() {
+        if (this.isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        if (this.size == 1) {
+            this.first = null;
+            this.last = null;
+            this.size--;
+            return;
+        }
+        Node current = this.first;
+        while (current != null) {
+            if (current.next == last) {
+                break;
+            }
+            current = current.next;
+        }
+        this.last = current;
+        this.last.next = null;
+        this.size--;
+    }
+
     // contains
+    public boolean contains(int data) {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        Node current = this.first;
+        while (current != null) {
+            if (current.value == data) {
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+
     // indexOf
+    public int indexOf(int data) {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        Node current = this.first;
+        int index = 0;
+        while (current != null) {
+            if (current.value == data) {
+                return index;
+            }
+            current = current.next;
+            index++;
+        }
+        return -1;
+    }
 
     // toArray
     public int[] toArray() {
-        int [] array = new int[this.size];
+        int[] array = new int[this.size];
         Node current = first;
         int index = 0;
-        while (current != null) 
-        {
+        while (current != null) {
             array[index] = current.value;
             index++;
             current = current.next;
@@ -72,13 +138,32 @@ public class LinkedList {
         return array;
     }
 
+    // toReverse [ 10 (previous) -> 20 (current) -> 30 (next) ] <--> [ 30 -> 20 ->
+    // 10 ]
+    public void toReverse() {
+        if (isEmpty()) {
+            return;
+        }
+        Node previousNode = this.first;
+        Node current = this.first.next;
+        while (current != null) {
+            Node next = current.next;
+            current.next = previousNode;
+            previousNode = current;
+            current = next;
+        }
+        last = first;
+        last.next = null;
+        first = previousNode;
+    }
+
     // toPrint
     public void toPrint() {
         Node current = first;
-        while (current != null) 
-        {
+        while (current != null) {
             System.out.print(current.value + " ");
             current = current.next;
         }
+        System.out.println();
     }
 }
--- DataStructures/DS_in_Java/src/linearDS/LinkedListDemo.java
+++ DataStructures/DS_in_Java/src/linearDS/LinkedListDemo.java
@@ -12,5 +12,34 @@ public class LinkedListDemo {
         list.toPrint();
         // int[] arr = list.toArray();
         // System.out.println(Arrays.toString(arr));
+        list.addFirst(5);
+        list.toPrint();
+        list.addFirst(1);
+        list.toPrint();
+        System.out.println(list.contains(10));
+        
+        list.addLast(40);
+        list.toPrint();
+        list.addLast(50);
+        list.toPrint();
+        System.out.println(list.indexOf(50));
+        
+        list.deleteFirst();
+        list.toPrint();
+        list.deleteFirst();
+        list.toPrint();
+        
+        list.deleteLast();
+        list.toPrint();
+        list.deleteLast();
+        list.toPrint();
+        
+        System.out.println(list.contains(5));
+        System.out.println(list.indexOf(50));
+        
+        System.out.println();
+        list.toPrint();
+        list.toReverse();
+        list.toPrint();
     }
 }
