6ff4fb44f71aa37cc6c15f2e1289d6e72fd4b7d7
Yuvraj <itsyuvi@outlook.com>
2023-05-17 08:57:40 +0530

    more on graphs

--- DataStructures/DS_in_JS/graph2.js
+++ DataStructures/DS_in_JS/graph2.js
@@ -69,6 +69,51 @@ class Graph {
             }
         }
     }
+
+    dfs(currentNode, visited) {
+        console.log(currentNode)
+        visited[currentNode] = true;
+
+        for (let i = 0; i < this.nodes[currentNode].length; i++) {
+            let e = this.nodes[currentNode][i];
+            if (!visited[e.des]) {
+                this.dfs(e.des, visited);
+            }
+        }
+    }
+
+    // Print All Paths
+    printAllPaths(currentNode, targetNode, path, visited) {
+        if (currentNode == targetNode) {
+            console.log(path);
+            return;
+        }
+        for (let i = 0; i < this.nodes[currentNode].length; i++) {
+            let e = this.nodes[currentNode][i];
+            if (!visited[e.des]) {
+                visited[currentNode] = true;
+                this.printAllPaths(e.des, targetNode, `${path}--${e.des}`, visited);
+                visited[currentNode] = false;
+            }
+        }
+    }
+
+    isCycleUndirected(currentNode, parentNode, visited) {
+        visited[currentNode] = true;
+
+        for (let i = 0; i < this.nodes[currentNode].length; i++) {
+            let e = this.nodes[currentNode][i];
+            if (visited[e.des] && e.des != parentNode) {
+                return true;
+            }
+            else if (!visited[e.des]) {
+                if (this.isCycleUndirected(e.des, currentNode, visited)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
 
 const graph1 = new Graph(4);
@@ -83,6 +128,31 @@ graph1.addEdge(3, 1, 1);
 graph1.addEdge(3, 2, 4);
 
 const graph2 = new Graph(7);
+
+/*
+    0   3
+     \ / \
+      2---1
+*/
+
+let visited = new Array(graph1.nodes.length);
+for (let i = 0; i < visited.length; i++) {
+    visited[i] = false;
+}
+graph1.dfs(0, visited)
+visited = new Array(graph1.nodes.length);
+for (let i = 0; i < visited.length; i++) {
+    visited[i] = false;
+}
+console.log(graph1.isCycleUndirected(0, -1, visited));
+
+/*  
+        1------3
+      /        | \
+    0          |  5----6
+      \        | /
+        2------4   
+*/
 
 graph2.addEdge(0, 1);
 graph2.addEdge(0, 2);
@@ -101,6 +171,20 @@ graph2.addEdge(5, 4);
 graph2.addEdge(5, 6);
 graph2.addEdge(6, 5);
 
+visited = new Array(graph2.nodes.length);
+for (let i = 0; i < visited.length; i++) {
+    visited[i] = false;
+}
+graph2.printAllPaths(0, 4, "0", visited);
+
+visited = new Array(graph2.nodes.length);
+for (let i = 0; i < visited.length; i++) {
+    visited[i] = false;
+}
+console.log(graph2.isCycleUndirected(0, -1, visited));
+
+const graph3 = new Graph(5);
+
 /*
     Disconnected Graph
     
@@ -110,8 +194,6 @@ graph2.addEdge(6, 5);
 
 */
 
-const graph3 = new Graph(5);
-
 graph3.addEdge(0, 1);
 graph3.addEdge(1, 0);
 graph3.addEdge(1, 2);
@@ -120,7 +202,7 @@ graph3.addEdge(3, 4);
 graph3.addEdge(4, 3);
 
 // For visiting disconnected graphs
-let visited = new Array(graph3.nodes.length);
+visited = new Array(graph3.nodes.length);
 for (let i = 0; i < visited.length; i++) {
     visited[i] = false;
 }
@@ -130,3 +212,9 @@ for (let i = 0; i < visited.length; i++) {
         graph3.disConnectedBFS(i, visited);
     }
 }
+
+visited = new Array(graph3.nodes.length);
+for (let i = 0; i < visited.length; i++) {
+    visited[i] = false;
+}
+console.log(graph3.isCycleUndirected(0, -1, visited));
