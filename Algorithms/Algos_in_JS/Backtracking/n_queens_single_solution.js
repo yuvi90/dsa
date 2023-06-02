a89427ed003157f958d0562d195daf3778a2b2da
Yuvraj <itsyuvi@outlook.com>
2023-06-02 14:10:13 +0530

    practice nqueens

--- Algorithms/Algos_in_JS/Backtracking/n_queens_single_solution.js
+++ Algorithms/Algos_in_JS/Backtracking/n_queens_single_solution.js
@@ -1,18 +1,18 @@
-function saveBoard(board, col, n) {
+function saveQueens(board, col, n) {
     // Base Condition
-    if (col >= n)
+    if (col === n)
         return true;
 
     for (let row = 0; row < n; row++) {
         // Constrains
         if (isSafe(board, row, col, n)) {
-            board[row][col] = 1;
-            // Recursion, col
-            if (saveBoard(board, col++, n)){
+            board[row][col] = 'Q';
+            // Recursion for col
+            if (saveQueens(board, col + 1, n)) {
                 return true;
             }
             // Backtrack
-            board[row][col] = 0;
+            board[row][col] = '.';
         }
     }
     return false;
@@ -23,20 +23,20 @@ function isSafe(board, row, col, n) {
 
     // Checks Horizontally, Left
     for (i = 0; i < col; i++) {
-        if (board[row][i] == 1) {
+        if (board[row][i] === 'Q') {
             return false;
         }
     }
 
     // Check Diagonally Upper Left
     for (i = row, j = col; i >= 0 && j >= 0; i--, j--) {
-        if (board[i][j] == 1) {
+        if (board[i][j] === 'Q') {
             return false;
         }
     }
     // Check Diagonally Lower Left
     for (i = row, j = col; i < n && j >= 0; i++, j--) {
-        if (board[i][j] == 1) {
+        if (board[i][j] === 'Q') {
             return false;
         }
     }
@@ -45,15 +45,15 @@ function isSafe(board, row, col, n) {
 }
 
 const board = [
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
+    ['.', '.', '.', '.'],
+    ['.', '.', '.', '.'],
+    ['.', '.', '.', '.'],
+    ['.', '.', '.', '.'],
 ]
 
 const n = board.length;
 
-if(saveBoard(board, 0, n)) {
+if (saveQueens(board, 0, n)) {
     console.log(board)
 } else {
     console.log("No Solution")
