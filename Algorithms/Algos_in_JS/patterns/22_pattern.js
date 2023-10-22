77cf7befbea8bdea1c5f043fe2301bd963e17953
Yuvraj <itsyuvi@outlook.com>
2023-10-22 17:49:49 +0530

    done some more questions

--- Algorithms/Algos_in_JS/patterns/22_pattern.js
+++ Algorithms/Algos_in_JS/patterns/22_pattern.js
@@ -11,14 +11,16 @@
 */
 
 function print(n) {
-  for (let i = 1; i < 2 * n; i++) {
+  for (let i = 0; i < 2 * n - 1; i++) {
     let line = "";
-    for (let j = 1; j < 2 * n; j++) {
-      if (i == 1 || i == 2 * n - 1 || j == 1 || j == 2 * n - 1) {
-        line += n;
-      } else {
-        line += j;
-      }
+    for (let j = 0; j < 2 * n - 1; j++) {
+      let topDistance = i;
+      let leftDistance = j;
+      let rightDistance = 2 * n - 2 - j;
+      let bottomDistance = 2 * n - 2 - i;
+
+      line +=
+        n - Math.min(topDistance, leftDistance, rightDistance, bottomDistance);
     }
     console.log(line);
   }
